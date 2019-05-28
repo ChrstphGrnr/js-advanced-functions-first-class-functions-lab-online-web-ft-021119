b83fb4fb3969106b41fc05bba77e07d9f848f45c
Christoph Gruener <christophgruener@me.com>
2019-05-28 15:10:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-function fetchSpecifiedDrivers(drivers, function) {
-  return function(drivers);
+function fetchSpecifiedDrivers(array, function) {
+  
 }
