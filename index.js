778c95963cf63e128ff395060526f940cc118910
Christoph Gruener <christophgruener@me.com>
2019-05-28 14:52:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // Code your solution in this file!
 
-const returnFirstTwoDrivers() {
-  const drivers = function (driversArray) {
+const returnFirstTwoDrivers = function (driversArray) {
     return driversArray.slice(0,1);
-  }
-  return drivers;
 }
